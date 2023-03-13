5d898970e860328626b77d832054467a0e36d583
yowmark <minkim.join@gmail.com>
2023-03-13 02:31:11 -0400

    save changes

--- Sources/FavOnboardingKit/FavOnboardingKit.swift
+++ Sources/FavOnboardingKit/FavOnboardingKit.swift
@@ -1,6 +1,24 @@
-public struct FavOnboardingKit {
-    public private(set) var text = "Hello, World!"
+import UIKit
+
+public class FavOnboardingKit {
+    public let text = "Hello, World!"
+    
+    private var onboardingViewController: OnboardingViewController = {
+        let controller = OnboardingViewController()
+        controller.modalTransitionStyle = .crossDissolve
+        controller.modalPresentationStyle = .fullScreen
+        return controller
+    }()
 
     public init() {
+    }
+    
+    public func launchOnboarding(rootVC: View) {
+        rootVC.present(onboardingViewController, animated: true, completion: nil)
+        
+    }
+    
+    public func dismissOnboarding() {
+        
     }
 }
---
+++ Sources/FavOnboardingKit/controller/OnboardingViewController.swift
@@ -0,0 +1,18 @@
+//
+//  File.swift
+//  
+//
+//  Created by MK on 2023-03-08.
+//
+
+import UIKit
+
+class OnboardingViewController : UIViewController {
+    
+    private var onboardingKit: FavOnboardingKit?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+}
